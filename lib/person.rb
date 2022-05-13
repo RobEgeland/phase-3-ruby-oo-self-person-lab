6399f5a7c1dc72b112cf9da0a337d30313fb5287
Robert Egeland <robegeland@yahoo.com>
2022-05-12 20:59:16 -0500

    commit tree 0831885e3ce052296b3600c0643bc5530d6f6772

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,83 @@
 # your code goes here
+
+class Person 
+    attr_reader :name, :bank_account, :happiness, :hygiene
+     
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+
+    def hygiene=(hygiene_num)
+        @hygiene = 
+            if hygiene_num > 10
+                10
+            elsif hygiene_num < 0
+                0
+            else
+                hygiene_num
+            end
+    end
+
+    def bank_account=(bank_account)
+        @bank_account = bank_account
+    end
+
+    def happiness=(happiness_num)
+        @happiness = 
+            if happiness_num > 10  
+                10
+            elsif happiness_num < 0
+                0
+            else 
+                happiness_num
+        end
+    end
+
+    def clean? 
+        self.hygiene > 7 ? true : false
+    end
+
+    def happy?
+        self.happiness > 7 ? true : false
+    end
+
+    def get_paid(amount)
+        self.bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
+end 
